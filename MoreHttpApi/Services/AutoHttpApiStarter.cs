fb3f320bd152f9cb7ac5f8375c6371b27f1b5e2c
Corey Jones <synik4l@gmail.com>
2026-04-06 22:02:19 -0500

    Removed proxy since theress already a mod that does this better

--- MoreHttpApi/Services/AutoHttpApiStarter.cs
+++ MoreHttpApi/Services/AutoHttpApiStarter.cs
@@ -11,18 +11,6 @@ public class AutoHttpApiStarter(HttpApi api, MSettings s) : IPostLoadableSinglet
         var port = (ushort)s.AutoStartPort.Value;
         api.SetPort(port);
         api.Start();
-
-        if (!s.AllowNetworkAccess.Value) { return; }
-
-        var networkPort = (ushort)s.NetworkPort.Value;
-        try
-        {
-            NetworkForwarder.Start(networkPort, port);
-        }
-        catch (Exception ex)
-        {
-            Debug.LogWarning($"[MoreHttpApi] Failed to start network forwarder: {ex.Message}");
-        }
     }
 
 }
--- MoreHttpApi/Services/MSettings.cs
+++ MoreHttpApi/Services/MSettings.cs
@@ -15,20 +15,10 @@ public class MSettings(ISettings settings, ModSettingsOwnerRegistry modSettingsO
         .CreateLocalized("LV.MHA.AutoStartPort")
         .SetLocalizedTooltip("LV.MHA.AutoStartPortDesc"));
 
-    public ModSetting<bool> AllowNetworkAccess { get; } = new(false, ModSettingDescriptor
-        .CreateLocalized("LV.MHA.AllowNetwork")
-        .SetLocalizedTooltip("LV.MHA.AllowNetworkDesc"));
-
-    public ModSetting<int> NetworkPort { get; } = new(8090, ModSettingDescriptor
-        .CreateLocalized("LV.MHA.NetworkPort")
-        .SetLocalizedTooltip("LV.MHA.NetworkPortDesc"));
-
     public override void OnBeforeLoad()
     {
         base.OnBeforeLoad();
         AutoStartPort.Descriptor.SetEnableCondition(() => AutoStartApi.Value);
-        AllowNetworkAccess.Descriptor.SetEnableCondition(() => AutoStartApi.Value);
-        NetworkPort.Descriptor.SetEnableCondition(() => AutoStartApi.Value && AllowNetworkAccess.Value);
     }
 
 }
